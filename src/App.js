f93063ed0d9733f72ed10625a722b97c718b8135
jnicolon <63628726+jnicolon@users.noreply.github.com>
2021-01-09 12:41:20 -0500

    variables and fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.scss';
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       Hello
+      <Home/>
     </div>
   );
 }
