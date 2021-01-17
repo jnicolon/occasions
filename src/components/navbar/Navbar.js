d7fcb250c0d5c48da5ba492f50726c98aeded793
jnicolon <63628726+jnicolon@users.noreply.github.com>
2021-01-17 15:57:00 -0500

    navbar display fix

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
   return (
     <div
       className="navbar"
-      style={!loggedIn ? { opacity: "none" } : { display: "block" }}
+      style={!loggedIn ? { display: "none" } : { display: "block" }}
     >
       <div className="navbar-container">
         <Link to="/">
